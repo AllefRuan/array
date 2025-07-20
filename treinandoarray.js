e5dc412be1269437e68afd2db4d277cac100e8e0
AllefRuan <rescola847@gmail.com>
2025-07-20 15:27:02 +0000

    trreinando

---
+++ treinandoarray.js
@@ -0,0 +1,10 @@
+let comidas = ['laranja', 'macarrão', 'feijão', 'arroz'];
+
+//adicionando elementos no meu array
+ comidas.push('frango');
+ comidas.push('pizza');
+ comidas.push('linguiça');
+
+console.log(comidas);
+
+
